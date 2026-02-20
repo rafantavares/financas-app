32ad3e01df63f716f8a096988ba4939078b50409
rafanuntavares <rafanuntavares@gmail.com>
2026-02-20 00:07:05 -0300

    estado e calculod e saldo

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,22 @@
 import {useState, useEffect} from 'react'
 import '/.index.css'
+
+const STORAGE_KEY = 'financas_dados'
+
+function formatReal(valor){
+  return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
+}
+
+function gerarId(){
+  return Date.now().toString()
+}
+
+export default function App(){
+  const [registros, setRegistros] = useState(() =>{
+    const salvo = localStorage.getItem(STORAGE_KEY)
+    return salvo ? JSON.parse(salvo): []
+  })
+
+  const [ganho, setGanho] = useState ({ descricao: '', valor: '', data: ''})
+  const [despesa, setDespesa] = useState ({descricao: '', valor: '', categoria: 'Alimentação', data: ''})
+}
